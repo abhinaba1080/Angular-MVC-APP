4f1787fe20ae4e5f47fbdf7a916aea47ae52ff35
abhinaba <abhinabaghosh.1994@gmail.com>
2017-03-03 15:58:46 -0500

    footer style _01

--- server/server.js
+++ server/server.js
@@ -27,6 +27,6 @@ app.get('/index.html',function(req,res){
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3003, function () {
+  console.log('Example app listening on port 3003!')
 });
